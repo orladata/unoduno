5a0090655f14dcb9cfbba6af83cdd8dd99baf74c
sonarycorporation <sonarycorporation@gmail.com>
2026-05-27 17:35:07 +0000

    fix: corrigindo redirecionamento do login para o dashboard

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,7 +4,7 @@ import { createClient } from '@/utils/supabase/server'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  const next = searchParams.get('next') ?? '/' // Default to Home if no next param
+  const next = searchParams.get('next') ?? '/dashboard' // Default to Dashboard if no next param
 
   if (code) {
     const supabase = await createClient()
--- app/login/actions.ts
+++ app/login/actions.ts
@@ -20,7 +20,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/')
+  redirect('/dashboard')
 }
 
 export async function signup(formData: FormData) {
@@ -38,7 +38,7 @@ export async function signup(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/')
+  redirect('/dashboard')
 }
 
 export async function signInWithGoogle() {
